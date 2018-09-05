0f7787346d74508508dfee01e4b16880f695845d
jwestelynck <20005089>
2018-09-05 15:38:05 +0200

    Fix WidthCredentials 1

--- pipeline.groovy
+++ pipeline.groovy
@@ -4,7 +4,7 @@ node{
 			credentialsId: '577e0c22-1f30-4ab0-b4f6-e65bd69fdd17',
 			usernameVariable: "GIT_USER",
 			passwordVariable: "GIT_PASSWORD"
-    )}{
+    )]{
 		stage('RETRIEVE'){
 			println env.GIT_USER
 			println env.GIT_PASSWORD
